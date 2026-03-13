d438801230a71219dcc2511e913eaa69f494f458
taufiqulhfz <denok7575@gmail.com>
2026-03-13 22:58:55 +0800

    Add files via upload

---
+++ POSTTEST_3/POSTTEST_3/Form2.vb
@@ -0,0 +1,18 @@
+﻿Public Class Form2
+
+    Private Sub Form2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub lblTelponKartu_Click(sender As Object, e As EventArgs) Handles lblTelponKartu.Click
+
+    End Sub
+
+    Private Sub lblTanggalKartu_Click(sender As Object, e As EventArgs) Handles lblTanggalKartu.Click
+
+    End Sub
+
+    Private Sub picKartu_Click(sender As Object, e As EventArgs) Handles picKartu.Click
+
+    End Sub
+End Class
